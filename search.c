0629d799e54f6904d77cdefc32d0bafaa6336112
Yaswanth-Tondepu <yaswanthtondepu@gmail.com>
2018-03-01 18:29:52 +0530

    Create search.c

---
+++ search.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() 
+{
+  int n,key,i;
+  //printf("Enter the no.of elements\n");
+  scanf("%d",&n);
+  //printf("Enter the element to be found\n");
+  scanf("%d",&key);
+  int a[n];
+  //printf("Enter the array elements\n");
+  for(i=0;i<n;i++)
+  {
+	    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+	  if(a[i]==key)
+	  {
+		    printf("%d",i);
+		    break;
+    }
+  }
+  if(i==n)
+  {
+  	printf("-1");
+  }
+  
+}
